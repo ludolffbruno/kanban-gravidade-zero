d1d4e5df2ecd8b87182b48362497ce9f7953acfb
Bruno Ludolff <brunoludolff@outlook.com>
2026-04-16 14:09:08 -0300

    fix: imports relativos no api/init-user

--- api/init-user.ts
+++ api/init-user.ts
@@ -1,5 +1,5 @@
-import { db } from "../db.js";
-import { verifyToken } from "../utils/auth.js";
+import { db } from "./db.js";
+import { verifyToken } from "./utils/auth.js";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
